61e01be94b379d8d21037afc0d3421976a6e5e4c
vlu <vladislav.naidyonov@yandex.ru>
2023-12-20 23:21:09 +0300

    before changing login form

---
+++ src/components/AddGameComponent/AddGame.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './login.css';
+import '../../css/base.css';
+
+const AddGame: React.FC = () => {
+    return (
+        <p>AddGame, виден только админам</p>
+    );
+};
+
+export default AddGame;
---
+++ src/pages/Calendar/Calendar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './login.css';
+import '../../css/base.css';
+
+const Calendar: React.FC = () => {
+    return (
+        <p>Календарь, виден всем зареганным</p>
+    );
+};
+
+export default Calendar;
